e01e33a047a99b79ef0ab32866396a496db6ef25
Denis <buh.borisov@yandex.ru>
2020-10-23 23:12:03 +0300

    Добавление строки в базу с помощью программы ruby

--- proga.rb
+++ proga.rb
@@ -3,7 +3,6 @@ require 'sqlite3'
 db = SQLite3::Database.new 'base.sqlite'
 
 db.execute "insert into cars (Name, Price) values ('Jaguar', 7777)"
-db.execute "insert into cars (Name, Price) values ('Opel', 7552)"
 
 
 
